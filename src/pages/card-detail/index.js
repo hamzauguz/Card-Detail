2b31205fcc497af0d854b7738063739f61cfcca8
hamzauguz <arastiranhamza@gmail.com>
2023-03-15 12:39:17 +0300

    cardnumber function

--- src/pages/card-detail/index.js
+++ src/pages/card-detail/index.js
@@ -1,11 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import leftPhoto from "../../images/bg-main-desktop.png";
 import "./Styles.CardDetail.css";
 
 const CardDetail = () => {
+  const [card, setCard] = useState("");
+
   useEffect(() => {
     document.body.className = "carddetail-body";
   }, []);
+  const handleCardDisplay = () => {
+    const rawText = [...card.split(" ").join("")]; // Remove old space
+    const creditCard = []; // Create card as array
+    rawText.forEach((t, i) => {
+      if (i % 4 === 0 && i !== 0) creditCard.push(" "); // Add space
+      creditCard.push(t);
+    });
+    return creditCard.join(""); // Transform card array to string
+  };
+
+  console.log(card);
   return (
     <div className="card-detail-main">
       <div className="cards-place">
@@ -16,7 +29,7 @@ const CardDetail = () => {
               <div className="entry-rounded" />
             </div>
             <div className="front-card-bottom">
-              <span className="card-number">0000 0000 0000 0000</span>
+              <span className="card-number">{card}</span>
               <div className="namewithdate">
                 <span>JANE APPLESEED</span>
                 <span>00/00</span>
@@ -27,7 +40,11 @@ const CardDetail = () => {
         <div id="back-card" className="back-card"></div>
       </div>
       <div>
-        <input placeholder="fd" />
+        <input
+          maxLength={19}
+          value={handleCardDisplay()}
+          onChange={(e) => setCard(e.target.value)}
+        />
       </div>
     </div>
   );
